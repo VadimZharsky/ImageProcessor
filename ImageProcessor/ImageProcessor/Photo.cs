66334f51997bcd03219e1181798e558ab15915ac
vadim <vadimzharsky1990@gmail.com>
2018-10-25 06:12:49 +0300

    fixed

---
+++ ImageProcessor/ImageProcessor/Photo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageProcessor
+{
+    class Photo
+    {
+        public string name { get; set; }
+        public Image image { get; set; }
+        public FileInfo fileInfo { get; set;}
+        public PropertyItem propertyItem { get; set; }
+    }
+}
--- ImageProcessor/ImageProcessor/Program.cs
+++ ImageProcessor/ImageProcessor/Program.cs
@@ -5,7 +5,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
+using System.Drawing.Imaging;
 
 namespace ImageProcessor
 {
@@ -13,19 +13,49 @@ namespace ImageProcessor
     {
         static void Main(string[] args)
         {
-            string sourceDir = @"D:\Home";
-            string destDir = @"D:\Home\newfolder";
-            CreateItems(sourceDir, destDir);
-            
+            string sourceDir = @"D:\c#works\it academy";
+            CreateItems(sourceDir);
+
             Console.ReadKey();
         }
 
-        private static void CreateItems(string sourceDir, string destDir)
+        private static void CreateItems(string sourceDir)
         {
-            DirectoryInfo dir = new DirectoryInfo(sourceDir);
-            List<FileInfo> filesInfos = new List<FileInfo>(dir.GetFiles());
+            var dir = new DirectoryInfo(sourceDir);
+            List<FileInfo> filesInfos = new List<FileInfo>(dir.GetFiles("*.jpg"));
+            List<Photo> photos = new List<Photo>();
+            DirectoryInfo parent = dir.Parent;
+            //parent.CreateSubdirectory("newFolder");
+            string destDir = $@"{parent.FullName}\newDossier";
             Directory.CreateDirectory(destDir);
-            Image newImage = Image.FromFile("SampImag.jpg");
+            foreach (FileInfo file in filesInfos)
+            {
+                var getImage = Image.FromFile(file.FullName);
+             
+                
+                photos.Add(new Photo() { fileInfo = file, name = file.Name, image = getImage });
+            }
+            foreach (Photo photo in photos)
+            {
+
+                try
+                {
+                    PropertyItem propItem = photo.image.GetPropertyItem(36867);
+                    string dateTaken = Encoding.UTF8.GetString(photo.propertyItem.Value);
+                    string newName = $"{Path.GetFileNameWithoutExtension(photo.fileInfo.Name)}_{dateTaken}{photo.fileInfo.Extension}";
+                    File.Copy($@"{dir.FullName}\{photo.fileInfo.Name}", $@"{destDir}\{newName}");
+                }
+                catch
+                {
+                    string dateTaken = photo.fileInfo.CreationTime.ToString();
+                    string newName = $"{Path.GetFileNameWithoutExtension(photo.fileInfo.Name)}{photo.fileInfo.Extension}";
+                    File.Copy($@"{dir.FullName}\{photo.fileInfo.Name}", $@"{destDir}\{newName}");
+                }
+                
+            }
+
+
+
 
             //foreach (FileInfo fileinfo in filesInfos)
             //{
@@ -35,6 +65,7 @@ namespace ImageProcessor
             //    string combine = Path.Combine(destDir, newName);
             //    fileinfo.CopyTo(combine);
             //}
+            
         }
     }
 }
